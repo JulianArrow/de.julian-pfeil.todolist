2f1fc50c71db4e126b6ae25b20475446709b61d2
Julian Pfeil <46976918+JulianArrow@users.noreply.github.com>
2024-09-06 15:16:19 +0200

    acpMenu to bootstrap-script

--- files_wcf/lib/bootstrap/de.julian-pfeil.todolist.php
+++ files_wcf/lib/bootstrap/de.julian-pfeil.todolist.php
@@ -1,12 +1,44 @@
 <?php
 
+use wcf\event\acp\menu\item\ItemCollecting;
 use wcf\system\event\EventHandler;
-use wcf\system\worker\event\RebuildWorkerCollecting;
+use wcf\system\menu\acp\AcpMenuItem;
+use wcf\event\worker\RebuildWorkerCollecting;
+use wcf\system\request\LinkHandler;
+use wcf\system\style\FontAwesomeIcon;
+use todolist\acp\page\CategoryListPage;
+use todolist\acp\form\CategoryAddForm;
+use wcf\system\WCF;
 
 return static function (): void {
     $eventHandler = EventHandler::getInstance();
 
     $eventHandler->register(RebuildWorkerCollecting::class, static function (RebuildWorkerCollecting $event) {
         $event->register(\todolist\system\worker\TodolistRebuildDataWorker::class, 0);
+    });
+
+    $eventHandler->register(ItemCollecting::class, static function (ItemCollecting $event) {
+        $event->register(new AcpMenuItem(
+            "todolist.acp.menu.link",
+            false,
+            'wcf.acp.menu.link.application'
+        ));
+
+        if (WCF::getSession()->getPermission('admin.todolist.general.canManageCategory')) {
+            $event->register(new AcpMenuItem(
+                "todolist.acp.menu.link.todo.category",
+                false,
+                'todolist.acp.menu.link',
+                LinkHandler::getInstance()->getControllerLink(CategoryListPage::class),
+            ));
+
+            $event->register(new AcpMenuItem(
+                "todolist.acp.menu.link.todo.category.add",
+                false,
+                'todolist.acp.menu.link.todo.category',
+                LinkHandler::getInstance()->getControllerLink(CategoryAddForm::class),
+                FontAwesomeIcon::fromString('plus;false')
+            ));
+        }
     });
 };
